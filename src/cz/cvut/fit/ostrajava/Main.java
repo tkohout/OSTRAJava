03b59f8b7935ab127d7a6a63e8ce8ac02471c30b
Tomas Kohout <email@tomaskohout.cz>
2015-10-15 18:53:46 +0200

    Initial commit

---
+++ src/cz/cvut/fit/ostrajava/Main.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception
+    {
+        Reader fr = null;
+        if (args.length == 2) {
+            fr = new InputStreamReader(new FileInputStream(new File(args[0])), args[1]);
+        }else if (args.length == 1) {
+            fr = new InputStreamReader(new FileInputStream(new File(args[0])));
+        }else {
+            System.out.println("Include filename in the arguments");
+            return;
+        }
+
+        JavaParser jp = new JavaParser(fr);
+        jp.CompilationUnit();
+    }
+}
